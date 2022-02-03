b16f76e6e2c81c57c716b9dbcf8db40b8a9c7285
Roman Nasyrov <romnasi@gmail.com>
2022-02-03 19:25:55 +0300

    feat: add base pages components

---
+++ src/components/text-book/text-book.tsx
@@ -0,0 +1,17 @@
+import Header from "../header/header";
+
+const TextBook: React.FC = () => {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <div className="container">
+          <h1>TextBook</h1>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default TextBook;
---
+++ src/const/const.ts
@@ -0,0 +1,6 @@
+export const enum AppRoute {
+  ROOT = "/",
+  AUDIO_CHALLENGE = "/audio-challenge",
+  SPRINT = "/sprint",
+  TEXTBOOK = "/textbook",
+}
