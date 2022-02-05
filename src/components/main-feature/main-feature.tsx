4c7cf4f2ac7bee36d053367fd52bcf33806ff377
Roman Nasyrov <romnasi@gmail.com>
2022-02-05 23:27:36 +0300

    feat: add styles for features screen

--- src/components/main-feature/main-feature.tsx
+++ src/components/main-feature/main-feature.tsx
@@ -1,48 +1,111 @@
+import { Link } from "react-router-dom";
+import { AppRoute } from "../../const/const";
+
 const MainFeature = () => {
   return (
     <section className="feature">
       <div className="feature__wrapper container">
-        <h2 className="feature__title">Описание возможностей и преимуществ приложения</h2>
+        <h2 className="feature__title visually-hidden">Описание возможностей и преимуществ приложения</h2>
         <ul className="feature__list">
           <li className="feature__item">
             <article className="feature__card">
               <h3 className="feature__item-title">Учебник</h3>
-              <p className="feature__item-text">
-                Более 3900 слов для изучения, с изображениями, разбитых на 
-                разделы по уровню твоей подготовки с удобной навигацией.
-              </p>
+              <div className="feature__card-wrapper">
+                <img 
+                  className="feature__image"
+                  width="400" 
+                  height="400" 
+                  src="assets/images/girl-lies.png" 
+                  alt="Читающая девушка" 
+                />
+                <p className="feature__item-text">
+                  Более 3900 слов с картинками для изучения, разбитых на 
+                  разделы по уровням подготовки с удобной навигацией
+                </p>
+                <Link className="btn" to={AppRoute.TEXTBOOK}>Попробовать</Link>
+              </div>
             </article>
           </li>
           
           <li className="feature__item">
             <article className="feature__card">
               <h3 className="feature__item-title">Список слов</h3>
-              <p className="feature__item-text">
-                Создай свой персональный список слов для изучения слов 
-                - добавляй слова, которым хочешь уделить особое 
-                внимание и удаляй, уже известные.
-              </p>
+              <div className="feature__card-wrapper">
+                <img 
+                  className="feature__image"
+                  width="400" 
+                  height="400" 
+                  src="assets/images/reading-girl.png" 
+                  alt="Читающая девушка" 
+                />
+                <p className="feature__item-text">
+                  Создай свой персональный список для изучения 
+                  - добавляй слова, которым хочешь уделить особое 
+                  внимание и удаляй уже известные
+                </p>
+                <Link className="btn" to={AppRoute.TEXTBOOK}>Попробовать</Link>
+              </div>
             </article>
           </li>
           
           <li className="feature__item">
             <article className="feature__card">
-              <h3 className="feature__item-title">Игры</h3>
-              <p className="feature__item-text">
-                Тебя ждут 2 удивительные игры на запоминание слов 
-                и восприятия на слух.
-              </p>
+              <h3 className="feature__item-title">Аудиовызов</h3>
+              <div className="feature__card-wrapper">
+                <img 
+                  className="feature__image feature__image--audio"
+                  width="450" 
+                  height="450" 
+                  src="assets/images/listening-girl.png" 
+                  alt="Девушка в наушниках" 
+                />
+                <p className="feature__item-text">
+                  Проверь, как быстро ты понимаешь на слух
+                  - слушай и изучай новые слова!
+                </p>
+              <Link className="btn" to={AppRoute.TEXTBOOK}>Попробовать</Link>
+              </div>
             </article>
           </li>
 
           <li className="feature__item">
             <article className="feature__card">
               <h3 className="feature__item-title">Статистика</h3>
-              <p className="feature__item-text">
-                Отслеживай свой прогресс в индивидуальной статистике, 
-                ставь цели и вдохновляйся на и достижение 
-                новых результатов каждый день!
-              </p>
+              <div className="feature__card-wrapper">
+                <img 
+                  className="feature__image"
+                  width="400" 
+                  height="400" 
+                  src="assets/images/stats.png" 
+                  alt="Девушка в наушниках" 
+                />
+                <p className="feature__item-text">
+                  Отслеживай свой прогресс в индивидуальной статистике, 
+                  ставь цели и вдохновляйся на достижение 
+                  новых результатов каждый день!
+                </p>
+                <Link className="btn" to={AppRoute.TEXTBOOK}>Попробовать</Link>
+              </div>
+            </article>
+          </li>
+
+          <li className="feature__item">
+            <article className="feature__card">
+              <h3 className="feature__item-title">Спринт</h3>
+              <div className="feature__card-wrapper">
+                <img 
+                  className="feature__image feature__image--audio"
+                  width="450" 
+                  height="450" 
+                  src="assets/images/listening-girl.png" 
+                  alt="Девушка в наушниках" 
+                />
+                <p className="feature__item-text">
+                  Испытай себя в знании английских слов, 
+                  зарабатывай очки и достигни лучшего результата в ответах на время.
+                </p>
+              <Link className="btn" to={AppRoute.SPRINT}>Попробовать</Link>
+              </div>
             </article>
           </li>
         </ul>
--- src/const/const.ts
+++ src/const/const.ts
@@ -4,3 +4,30 @@ export const enum AppRoute {
   SPRINT = "/sprint",
   TEXTBOOK = "/textbook",
 }
+
+export const teamMembers = [
+  {
+    id: 1,
+    name: "Александр Голамонов",
+    work: "Здесь будет написано, что делал каждый из нас. Если хотите текст можете предложить уже сейчас",
+    ghLink: "https://github.com/Todd89",
+    avatar: "assets/images/avatar-sasha.jpg",
+    role: "Team-lead",
+  },
+  {
+    id: 2,
+    name: "Ольга Якушева",
+    work: "Здесь будет написано, что делал каждый из нас. Если хотите текст можете предложить уже сейчас",
+    ghLink: "https://github.com/Dairin-dei",
+    avatar: "assets/images/avatar-olya.jpg",
+    role: "Front-end",
+  },
+  {
+    id: 3,
+    name: "Роман Гольцман",
+    work: "Здесь будет написано, что делал каждый из нас. Если хотите текст можете предложить уже сейчас",
+    ghLink: "https://github.com/Romnasi",
+    avatar: "assets/images/avatar-roman.jpg",
+    role: "Front-end",
+  }
+]
