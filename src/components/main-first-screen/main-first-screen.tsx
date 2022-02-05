97d4eee06ddb74fd82d9ac4d7b56b03694429419
Roman Nasyrov <romnasi@gmail.com>
2022-02-05 23:26:54 +0300

    feat: add styles for first screen main page

--- src/components/main-first-screen/main-first-screen.tsx
+++ src/components/main-first-screen/main-first-screen.tsx
@@ -2,13 +2,26 @@ const MainFirstScreen: React.FC = () => {
   return (
     <section className="first-screen">
       <div className="first-screen__wrapper container">
-        <h1>Получите неописуемое веселье от изучения английского!</h1>
-        <img 
-          src="https://image.freepik.com/free-psd/3d-girl-character-jumping-air_23-2149086089.jpg" 
-          width="360" 
-          height="360"  
-          alt="Счастливый студент после RS Lang" 
-        />
+        <div>
+          <h1 className="first-screen__title">
+            Изучайте Английский весело!
+          </h1>
+          <p className="first-screen__text">
+            RS Lang позволит вам узнать тонну новых слов,
+            запомнить произношение и весело провести время
+          </p>
+        </div>
+
+        <div className="first-screen__image-wrapper">
+          <img 
+            className="first-screen__image"
+            src="assets/images/jumping-girl.png"
+            width="287"
+            height="287"  
+            alt="Счастливый студент после RS Lang" 
+          />
+        </div>
+        
       </div>
     </section>
   );
