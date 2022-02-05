c4a87f93516fbca940ce66795db4adbcd67e5727
Roman Nasyrov <romnasi@gmail.com>
2022-02-06 00:36:07 +0300

    feat: add stats component

---
+++ src/components/stats/stats.tsx
@@ -0,0 +1,14 @@
+import Footer from "../footer/footer";
+import Header from "../header/header";
+
+const Stats:React.FC = () => {
+  return (
+    <>
+      <Header />
+      <h1>Stats</h1>
+      <Footer />
+    </>
+  );
+}
+
+export default Stats;
