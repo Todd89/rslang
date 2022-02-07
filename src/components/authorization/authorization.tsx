28fb76c51f6cb523b158d5bce4d2731d8d74cd9d
Roman Nasyrov <romnasi@gmail.com>
2022-02-07 22:34:21 +0300

    feat: add Authorization component

---
+++ src/components/authorization/authorization.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+interface AuthorizationComponentProps {
+  isRegistration: boolean;
+  changeForm: (evt: React.MouseEvent) => void;
+  toggleForm: () => void;
+}
+
+const Authorization: React.FC<AuthorizationComponentProps> = ({ isRegistration, changeForm, toggleForm }) => {
+  
+  return (
+    <>
+      <div className="modal authorization" id="modal">
+        <button 
+            className="modal__close authorization__close"
+            onClick={() => toggleForm()}
+        >
+          <span className="visually-hidden">
+            Закрыть форму
+          </span>
+        </button>
+        <div className="modal__guts">
+
+          <form className="authorization__form" autoComplete="off">
+            <h3 className="authorization__title">
+              {isRegistration ? "Регистрация" : "Войдите в аккаунт"} 
+            </h3> 
+            <ul className="authorization__list">
+              <li className="authorization__item">
+                <label className="authorization__label" htmlFor="email">
+                  Введите почту
+                </label>
+                <input 
+                    className="authorization__control" 
+                    id="email" 
+                    type="text"
+                    placeholder="kirkorov@mail.ru"
+                    autoFocus
+                />
+              </li>
+
+              <li className="authorization__item">
+                <label className="authorization__label" htmlFor="parol">
+                  Введите пароль
+                </label>
+                <input 
+                    className="authorization__control" 
+                    id="parol" 
+                    type="password" 
+                />
+              </li>
+            </ul>
+
+            <div className="authorization__buttons-wrapper">
+              <button type="button" className="btn authorization__btn">
+                {isRegistration ? "Зарегистрироваться" : "Войти"}
+              </button>
+              <div className="authorization__question">
+                <span className="authorization__question-text">
+                  {isRegistration ? "Уже зарегистрированы?" : "Еще нет аккаунта?"}
+                </span> 
+                <button 
+                  className="authorization__question-btn"
+                  onClick={(evt) => changeForm(evt)}
+                >
+                  {isRegistration ? "Войдите" : "Зарегистрируйтесь"}
+                </button>
+              </div>
+            </div>
+
+          </form>
+        </div>
+      </div>
+
+      <div className="modal__overlay" id="modal-overlay"></div>
+    </>
+  );
+}
+
+export default Authorization;
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,53 +1,80 @@
+import Authorization from "../authorization/authorization";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { AppRoute } from '../../const/const';
 
 const Header: React.FC = () => {
-  const [isOpenNav, setIsOpenNav] = useState(false)
+  const [isOpenNav, setIsOpenNav] = useState(false);
+  const [isFormOpen, setIsFormOpen] = useState(false);
+  const [isRegistration, setIsRegistration] = useState<boolean>(false);
+
+  const changeForm = (evt: React.MouseEvent) => {
+    evt.preventDefault();
+    setIsRegistration((prev) => !prev)
+  };
+
+  const toggleForm = () => {
+    setIsFormOpen((prev) => !prev);
+  }
 
   return (
-    <header className="header">
-      <div className="header__wrapper container">
-        <Link className="header__logo logo" to={AppRoute.ROOT}>
-          RS Lang
-        </Link>
-        <nav className={isOpenNav ? "header__nav nav nav--open" : "header__nav nav" }>
-            <ul className="nav__list">
-                <li className="nav__item">
-                  <Link to={AppRoute.TEXTBOOK} className="nav__link nav__link--active">
-                    Учебник
-                  </Link>
-                </li>
-                <li className="nav__item">
-                  <Link to={AppRoute.AUDIO_CHALLENGE} className="nav__link">
-                    Аудиовызов
-                  </Link>
-                </li>
-                <li className="nav__item">
-                  <Link to={AppRoute.SPRINT} className="nav__link">
-                    Спринт
-                  </Link>
-                </li>
-                
-                <li className="nav__item">
-                  <Link to={AppRoute.STATS} className="nav__link">
-                    Статистика
-                  </Link>
-                </li>
-            </ul>
-        </nav>
-
-        <button 
-            className="menu-toggle" 
-            type="button"
-            onClick={() => {setIsOpenNav((prev) => !prev)}}
-        >
-          <span className="visually-hidden">Toggle button</span>
-        </button>
-
-        <button className="btn header__login">Войти</button>
-      </div>
-    </header>
+    <>
+      {
+        isFormOpen && 
+        <Authorization 
+          isRegistration={isRegistration}
+          changeForm={changeForm}
+          toggleForm={toggleForm}  
+        />
+      }
+      <header className="header">
+        <div className="header__wrapper container">
+          <Link className="header__logo logo" to={AppRoute.ROOT}>
+            RS Lang
+          </Link>
+          <nav className={isOpenNav ? "header__nav nav nav--open" : "header__nav nav" }>
+              <ul className="nav__list">
+                  <li className="nav__item">
+                    <Link to={AppRoute.TEXTBOOK} className="nav__link nav__link--active">
+                      Учебник
+                    </Link>
+                  </li>
+                  <li className="nav__item">
+                    <Link to={AppRoute.AUDIO_CHALLENGE} className="nav__link">
+                      Аудиовызов
+                    </Link>
+                  </li>
+                  <li className="nav__item">
+                    <Link to={AppRoute.SPRINT} className="nav__link">
+                      Спринт
+                    </Link>
+                  </li>
+                  
+                  <li className="nav__item">
+                    <Link to={AppRoute.STATS} className="nav__link">
+                      Статистика
+                    </Link>
+                  </li>
+              </ul>
+          </nav>
+  
+          <button 
+              className="menu-toggle" 
+              type="button"
+              onClick={() => {setIsOpenNav((prev) => !prev)}}
+          >
+            <span className="visually-hidden">Toggle button</span>
+          </button>
+  
+          <button 
+            className="btn header__authorization"
+            onClick={() => toggleForm()}
+          >
+            Войти
+          </button>
+        </div>
+      </header>
+    </>
   );
 }
 
